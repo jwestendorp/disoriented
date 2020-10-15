127208b25934b91006e89aaef0f7c2cbf1953941
Jonas <tuberjonas@gmail.com>
2020-10-15 19:23:45 +0200

    init

---
+++ app/controller/main.js
@@ -0,0 +1,55 @@
+let socket = io();
+
+let containerDiv = document.querySelector(".cursor-container");
+
+function mouseHasMoved(event) {
+  let x = event.clientX;
+  let y = event.clientY;
+
+  socket.emit("cursor-moved", {
+    x: x,
+    y: y,
+  });
+}
+
+document.body.addEventListener("mousemove", mouseHasMoved);
+
+function cursorsChanged(cursors) {
+  let allCursorDivs = containerDiv.querySelectorAll(".cursor");
+
+  for (let i = 0; i < allCursorDivs.length; i++) {
+    allCursorDivs[i].dataset.updated = false;
+  }
+
+  let ids = Object.keys(cursors);
+
+  for (let i = 0; i < ids.length; i++) {
+    let id = ids[i];
+
+    let cursorDiv = containerDiv.querySelector('.cursor[data-id="' + id + '"]');
+
+    if (cursorDiv == null) {
+      // There's no div on the page with that ID yet.
+      cursorDiv = document.createElement("div");
+      cursorDiv.classList.add("cursor");
+      cursorDiv.dataset.id = id;
+
+      containerDiv.appendChild(cursorDiv);
+    }
+
+    // cursorDiv.style.left = cursors[id].x + "px";
+    // cursorDiv.style.top = cursors[id].y + "px";
+
+    cursorDiv.innerHTML = "X: " + cursors[id].x + ", Y: " + cursors[id].y;
+
+    cursorDiv.dataset.updated = true;
+  }
+
+  for (let i = 0; i < allCursorDivs.length; i++) {
+    if (allCursorDivs[i].dataset.updated == "false") {
+      allCursorDivs[i].parentNode.removeChild(allCursorDivs[i]);
+    }
+  }
+}
+
+socket.on("cursor-object-changed", cursorsChanged);
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
+let cursors = {};
+
+function newSocketConnection(socket) {
+  cursors[socket.id] = {
+    x: 0,
+    y: 0,
+  };
+
+  function socketDisconnected() {
+    delete cursors[socket.id];
+  }
+
+  function cursorMoved(coordinate) {
+    cursors[socket.id] = coordinate;
+
+    io.emit("cursor-object-changed", cursors);
+  }
+
+  socket.on("disconnect", socketDisconnected);
+  socket.on("cursor-moved", cursorMoved);
+}
+
+io.on("connection", newSocketConnection);
+
+app.use("/canvas", express.static("./app/canvas"));
+app.use("/", express.static("./app/controller"));
+
+server.listen(8080);
